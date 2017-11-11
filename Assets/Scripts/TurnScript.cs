6805d932cd9a5da0511afc29ced8f67a3f45548c
Bharat Gudihal <bharatgudihal@users.noreply.github.com>
2017-11-10 22:56:10 -0700

    initial cog and bar setup

---
+++ Assets/Scripts/TurnScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnScript : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.MoveRotation(Quaternion.Euler(transform.rotation.eulerAngles.x + 0.1f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z));
+        }
+	}
+}
